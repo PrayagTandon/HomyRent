bc60ab1f01809b0709a636bf06852f8f638d307a
Prayag <tandonprayag99@gmail.com>
2025-06-07 20:14:17 -0400

    ...

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,11 +9,11 @@ const Navbar = () => {
       className="fixed top-0 left-0 w-full z-50 shadow-xl"
       style={{ height: `${NAVBAR_HEIGHT}px` }}
     >
-      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-400 text-white">
+      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
         <div className="flex items-center gap-4 md:gap-6">
           <Link
             href="/"
-            className="cursor-pointer hover:text-primary-300"
+            className="cursor-pointer hover:!text-primary-300"
             scroll={false}
           >
             <div className="flex items-center gap-3">
@@ -26,12 +26,15 @@ const Navbar = () => {
               />
               <div className="text-xl font-bold">
                 HOMY
-                <span className="text-secondary-500 font-light hover:text-primary-300">
+                <span className="text-secondary-500 font-light hover:!text-primary-300">
                   RENT
                 </span>
               </div>
             </div>
           </Link>
+          <p className="text-primary-200 hidden md:block">
+            Discover your perfect rental apartment with our advanced search
+          </p>
         </div>
       </div>
     </div>
