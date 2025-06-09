1abec4d62c7130cb404dacf49a8b9f6d5f5fd776
Prayag <tandonprayag99@gmail.com>
2025-06-08 23:09:30 -0400

    Starting the Discover section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
 
 const Landing = () => {
   return (
     <div> 
-      <HeroSection/>
+      <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
     </div>
   )
 }
