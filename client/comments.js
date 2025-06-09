c8a50837b16ee2734592c2c1077670ae44d99177
Prayag <tandonprayag99@gmail.com>
2025-06-08 20:55:29 -0400

    ...

--- client/comments.js
+++ client/comments.js
@@ -1,3 +1,5 @@
 /*
     1. We use () to name a folder, when we don't want it to be used in routing, in Next.js
+
+    2. We use "use client" whenever we want to use framer motion in our file, especially [<motion.div></motion.div>]
 */
--- client/src/app/(nondashboard)/landing/FeaturesSection.tsx
+++ client/src/app/(nondashboard)/landing/FeaturesSection.tsx
@@ -1,9 +1,37 @@
+"use client";
+
 import React from 'react'
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+    hidden: { opacity: 0, y: 50},
+    visible: { 
+        opacity: 1, 
+        y: 0,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.2
+        }
+    }
+};
+
+const itemVariants = {
+    hidden: { opacity: 0, y: 20},
+    visible: { opcaity: 1, y: 0}
+};
 
 const FeaturesSection = () => {
   return (
-    <div>FeaturesSection</div>
+    <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: true}}
+        variants={containerVariants}
+        className=" py-24 px-6 sm:px-8 lg:px-12 xl:px-16 bg-white"
+    >
+        FeaturesSection
+    </motion.div>
   )
 }
 
-export default FeaturesSection
+export default FeaturesSection;
