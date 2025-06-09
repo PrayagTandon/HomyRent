4a2e26bd7fc236720969e935ab515ac703446947
Prayag <tandonprayag99@gmail.com>
2025-06-09 00:27:48 -0400

    Developing the Footer section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from './HeroSection'
 import FeaturesSection from './FeaturesSection'
 import DiscoverSection from './DiscoverSection'
 import CallToActionSection from './CallToActionSection'
+import Footer from './Footer'
 
 const Landing = () => {
   return (
@@ -11,6 +12,7 @@ const Landing = () => {
       <FeaturesSection />
       <DiscoverSection />
       <CallToActionSection />
+      <Footer />
     </div>
   )
 }
