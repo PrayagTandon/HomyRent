ffaf76d39276b6566677a66843f0f3e8522922a7
Prayag <tandonprayag99@gmail.com>
2025-06-28 19:07:47 -0400

    Setting up the middleware for authentication

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response ,NextFunction } from "express";
-import { JwtPayload } from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 interface DecodedToken extends JwtPayload {
     sub: string;
@@ -17,8 +17,31 @@ declare global {
     }
 }
 
-export const authMiddleware = (allowedRules: string[]) => {
+export const authMiddleware = (allowedRoles: string[]) => {
     return (req: Request, res: Response, next: NextFunction): void => {
         const token = req.headers.authorization?.split(" ")[1];
+
+        if(!token) {
+            res.status(401).json({ message: "Unauthorized" });
+            return;
+        }
+
+        try {
+            const decoded = jwt.decode(token) as DecodedToken
+            const userRole = decoded["custom:role"] || "";
+            req.user = {
+                id: decoded.sub,
+                role: userRole
+            }
+
+            const hasAccess = allowedRoles.includes(userRole.toLowerCase());
+            if(!hasAccess) {
+                res.status(403).json({ message: "Access Denied" });
+                return;
+            }
+        } catch(error) {
+            console.error(`Failed to decode token: ${error}`);
+            res.status(400).json({ message: "Invalid token" })
+        }
     }
 }
