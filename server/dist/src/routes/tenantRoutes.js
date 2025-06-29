4c01a8cf34a8a21a567fcb62c7aafc22762b2740
Prayag <tandonprayag99@gmail.com>
2025-06-28 20:34:47 -0400

    Setting up teh controllers for Tennat and Maager routes

--- server/dist/src/routes/tenantRoutes.js
+++ server/dist/src/routes/tenantRoutes.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const tenantControllers_1 = require("../controllers/tenantControllers");
 const router = express_1.default.Router();
-router.get("/");
+router.get("/:cognitoId", tenantControllers_1.getTenant);
+router.post("/", tenantControllers_1.createTenant);
 exports.default = router;
